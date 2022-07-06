8e05e45716010b48c2f3870fb0867bd82da55238
Daniel Kubong <kadmofor@gmail.com>
2022-07-05 23:59:55 -0400

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Daniel Kubong on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
