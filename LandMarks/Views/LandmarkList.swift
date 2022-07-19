0258d6c64316cecbfd21a2b5d48b5da2ff4518e0
Daniel Kubong <kadmofor@gmail.com>
2022-07-19 16:59:59 -0400

    Worked on generating previews dynamically, added Details to the List and Child pages

--- LandMarks/Views/LandmarkList.swift
+++ LandMarks/Views/LandmarkList.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView{
+            List(landmarks) { landmark in
+                NavigationLink{
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
-        
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 12 mini", "iPhone 11", "iPhone 13 Pro Max", "iPad Pro (12.9-inch) (5th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+            .previewDisplayName(deviceName)
+        }
     }
 }
