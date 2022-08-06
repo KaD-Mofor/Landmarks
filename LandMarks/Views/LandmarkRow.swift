3fe64fbbe9b5f146f32b10a80a481e1a35a71ed1
Daniel Kubong <kadmofor@gmail.com>
2022-08-06 14:41:29 -0400

    Add favorite btn and toggle btn

--- LandMarks/Views/LandmarkRow.swift
+++ LandMarks/Views/LandmarkRow.swift
@@ -21,6 +21,11 @@ struct LandmarkRow: View {
             Text(landmark.name)
             
             Spacer()
+            
+            if landmark.isFavorite {
+                Image (systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
