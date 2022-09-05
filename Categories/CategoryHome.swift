1fcf894de033a13d63719ea01c61b7bfc78ac371
Daniel Kubong <kadmofor@gmail.com>
2022-09-05 10:07:36 -0400

    Add feature and list, navigationlink and view

--- Categories/CategoryHome.swift
+++ Categories/CategoryHome.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    //Create evironmentObject variable
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //Add Navigation views and using with .navigationTile
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                
+                ForEach(
+                    modelData.categories.keys.sorted(), id: \.self) {
+                        key in CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                    }
+                    .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        }
     }
 }
 
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            //Calling environment var
+            .environmentObject(ModelData())
     }
 }
