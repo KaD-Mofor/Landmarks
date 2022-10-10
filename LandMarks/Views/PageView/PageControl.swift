49578613caae6426b3e85f118bfe95e02ebadf9a
Daniel Kubong <kadmofor@gmail.com>
2022-10-10 01:44:29 -0400

    Add a Custom Page Control

---
+++ LandMarks/Views/PageView/PageControl.swift
@@ -0,0 +1,20 @@
+//
+//  PageControl.swift
+//  LandMarks
+//
+//  Created by Daniel Kubong on 10/10/22.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageControl_Previews: PreviewProvider {
+    static var previews: some View {
+        PageControl()
+    }
+}
